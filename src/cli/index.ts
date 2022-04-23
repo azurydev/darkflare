f742661f2311fb5d2baf61647cb1979f9c087a76
Azury <azury.wtf@gmail.com>
2022-04-23 13:42:02 +0200

    added main file for cli

---
+++ src/cli/index.ts
@@ -0,0 +1,14 @@
+#!/usr/bin/env node
+
+import { hideBin } from 'yargs/helpers'
+import yargs from 'yargs'
+import build from './modules/build'
+
+yargs(hideBin(process.argv))
+  .command('*', 'Create a production-ready build.', () => {}, (args) => build(process.cwd(), args.dev as boolean))
+  .option('dev', {
+    type: 'boolean',
+    description: 'Run Darkflare in development mode.'
+  })
+  .demandCommand(0)
+  .parse()
