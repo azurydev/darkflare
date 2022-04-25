3005ff0001113116f9a3e54da7c6dc0e5821622f
Azury <azury.wtf@gmail.com>
2022-04-25 18:20:10 +0200

    added fallback config

--- src/cli/modules/loadConfig.ts
+++ src/cli/modules/loadConfig.ts
@@ -1,6 +1,15 @@
 import { readFile } from 'fs/promises'
 
 export default async (directory: string) => {
-  const config = await readFile(`${directory}/darkflare.json`, { encoding: 'utf-8' })
-  return JSON.parse(config)
+  const raw = await readFile(`${directory}/darkflare.json`, { encoding: 'utf-8' })
+  , config = JSON.parse(raw)
+
+  return {
+    base: config.base || '/',
+    origin: config.origin || '*',
+    protect: {
+      strict: config.protect && config.protect.strict ? config.protect.strict : [],
+      flexible: config.flexible && config.protect.flexible ? config.protect.flexible : []
+    }
+  }
 }
