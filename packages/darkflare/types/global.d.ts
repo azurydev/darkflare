c016713596370a6372f19ae48646903f937f0211
Azury <azury.wtf@gmail.com>
2022-05-18 00:03:00 +0200

    released `v2`

--- packages/darkflare/types/global.d.ts
+++
@@ -1,7 +0,0 @@
-type DarkflareRequest = import('./modules/DarkflareRequest')
-
-type DarkflareResponse = import('./modules/DarkflareResponse')
-
-type Handler = import('./modules/Handler')
-
-type Middleware = import('./modules/Middleware')
