df030c2a82e86a0dce4e34d69fed87cdc691fd20
Azury <azury.wtf@gmail.com>
2022-05-17 18:30:57 +0200

    added global types

---
+++ packages/darkflare/types/global.d.ts
@@ -0,0 +1,7 @@
+type DarkflareRequest = import('./modules/DarkflareRequest')
+
+type DarkflareResponse = import('./modules/DarkflareResponse')
+
+type Handler = import('./modules/Handler')
+
+type Middleware = import('./modules/Middleware')
