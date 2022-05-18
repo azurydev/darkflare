c1a345ef546f87d237f6fc4d18c1be07d6a9f6c0
Azury <azury.wtf@gmail.com>
2022-05-18 23:32:56 +0200

    fixed build process

--- packages/darkflare/src/modules/createRoute.ts
+++ packages/darkflare/src/modules/createRoute.ts
@@ -1,8 +1,7 @@
 import { build } from 'esbuild'
-import { join } from 'path'
 import getClosestMiddleware from './getClosestMiddleware'
 
-export default async (absolutePath: string, directory: string, middlewares: MiddlewareObject[], config: Configuration, modules: boolean) => {
+export default async (absolutePath: string, middlewares: MiddlewareObject[], config: Configuration, modules: boolean) => {
   let importsString = ''
   , routesString = ''
 
@@ -32,24 +31,23 @@ export default async (absolutePath: string, directory: string, middlewares: Midd
     .replace('.ts', '')
     .replace('/index', '')
 
-  // compile route to commonjs
-  , pathToCommonJsModule = absolutePath.replace(join(directory, './src'), join(directory, './.darkflare/.cache')).replace('.ts', '.cjs')
+  , compilePath = absolutePath
+    .substring(absolutePath.indexOf('routes'))
+    .replaceAll('\\', '/')
+    .replaceAll('\/', '/')
 
-  await build({
+  // compile route to commonjs
+  const result = await build({
     entryPoints: [absolutePath],
-    format: 'cjs',
-    bundle: true,
-    minify: true,
-    outfile: pathToCommonJsModule
+    format: 'esm',
+    metafile: true,
+    write: false
   })
 
-  const m = require(pathToCommonJsModule)
+  const outputs = result.metafile?.outputs
 
-  // ignore route if it doesn't export a handler
-  if (typeof m !== 'object' || typeof m.default !== 'object') return
-
-  // added route to imports string
-  importsString += `import ${name} from "../src/${importPath.replace('/index', '')}"\n`
+  // @ts-ignore
+  const exportedModules = outputs[/[^/]*$/.exec(compilePath)[0].replace('.ts', '.js')].exports
 
   let methods = ''
 
@@ -58,69 +56,81 @@ export default async (absolutePath: string, directory: string, middlewares: Midd
 
   // add endpoints to routes string
   if (modules) {
-    if (typeof m.default.get === 'function') {
-      methods += 'GET, '
-      routesString += `.get('${endpoint}', async (request: IttyRequest, env: any, context: any) => await handler(request, ${name}.get, env, context${closestMiddleware ? `, ${closestMiddleware.name}` : ''}))`
+    if (exportedModules.includes('Get')) {
+      methods += 'Get, '
+      routesString += `.get('${endpoint}', async (request: IttyRequest, env: any, context: any) => await handler(request, ${name}_Get, env, context${closestMiddleware ? `, ${closestMiddleware.name}` : ''}))`
     }
   
-    if (typeof m.default.post === 'function') {
-      methods += 'POST, '
-      routesString += `.post('${endpoint}', async (request: IttyRequest, env: any, context: any) => await handler(request, ${name}.post, env, context${closestMiddleware ? `, ${closestMiddleware.name}` : ''}))`
+    if (exportedModules.includes('Post')) {
+      methods += 'Post, '
+      routesString += `.post('${endpoint}', async (request: IttyRequest, env: any, context: any) => await handler(request, ${name}_Post, env, context${closestMiddleware ? `, ${closestMiddleware.name}` : ''}))`
     }
   
-    if (typeof m.default.patch === 'function') {
-      methods += 'PATCH, '
-      routesString += `.patch('${endpoint}', async (request: IttyRequest, env: any, context: any) => await handler(request, ${name}.patch, env, context${closestMiddleware ? `, ${closestMiddleware.name}` : ''}))`
+    if (exportedModules.includes('Patch')) {
+      methods += 'Patch, '
+      routesString += `.patch('${endpoint}', async (request: IttyRequest, env: any, context: any) => await handler(request, ${name}_Patch, env, context${closestMiddleware ? `, ${closestMiddleware.name}` : ''}))`
     }
   
-    if (typeof m.default.put === 'function') {
-      methods += 'PUT, '
-      routesString += `.put('${endpoint}', async (request: IttyRequest, env: any, context: any) => await handler(request, ${name}.put, env, context${closestMiddleware ? `, ${closestMiddleware.name}` : ''}))`
+    if (exportedModules.includes('Put')) {
+      methods += 'Put, '
+      routesString += `.put('${endpoint}', async (request: IttyRequest, env: any, context: any) => await handler(request, ${name}_Put, env, context${closestMiddleware ? `, ${closestMiddleware.name}` : ''}))`
     }
   
-    if (typeof m.default.delete === 'function') {
-      methods += 'DELETE, '
-      routesString += `.delete('${endpoint}', async (request: IttyRequest, env: any, context: any) => await handler(request, ${name}.delete, env, context${closestMiddleware ? `, ${closestMiddleware.name}` : ''}))`
+    if (exportedModules.includes('Delete')) {
+      methods += 'Delete, '
+      routesString += `.delete('${endpoint}', async (request: IttyRequest, env: any, context: any) => await handler(request, ${name}_Delete, env, context${closestMiddleware ? `, ${closestMiddleware.name}` : ''}))`
     }
   
-    if (typeof m.default.head === 'function') {
-      methods += 'HEAD, '
-      routesString += `.head('${endpoint}', async (request: IttyRequest, env: any, context: any) => await handler(request, ${name}.head, env, context${closestMiddleware ? `, ${closestMiddleware.name}` : ''}))`
+    if (exportedModules.includes('Head')) {
+      methods += 'Head, '
+      routesString += `.head('${endpoint}', async (request: IttyRequest, env: any, context: any) => await handler(request, ${name}_Head, env, context${closestMiddleware ? `, ${closestMiddleware.name}` : ''}))`
     }
   } else {
-    if (typeof m.default.get === 'function') {
-      methods += 'GET, '
-      routesString += `.get('${endpoint}', async (request: IttyRequest) => await handler(request, ${name}.get, null, null${closestMiddleware ? `, ${closestMiddleware.name}` : ''}))`
+    if (exportedModules.includes('Get')) {
+      methods += 'Get, '
+      routesString += `.get('${endpoint}', async (request: IttyRequest) => await handler(request, ${name}_Get, null, null${closestMiddleware ? `, ${closestMiddleware.name}` : ''}))`
     }
   
-    if (typeof m.default.post === 'function') {
-      methods += 'POST, '
-      routesString += `.post('${endpoint}', async (request: IttyRequest) => await handler(request, ${name}.post, null, null${closestMiddleware ? `, ${closestMiddleware.name}` : ''}))`
+    if (exportedModules.includes('Post')) {
+      methods += 'Post, '
+      routesString += `.post('${endpoint}', async (request: IttyRequest) => await handler(request, ${name}_Post, null, null${closestMiddleware ? `, ${closestMiddleware.name}` : ''}))`
     }
   
-    if (typeof m.default.patch === 'function') {
-      methods += 'PATCH, '
-      routesString += `.patch('${endpoint}', async (request: IttyRequest) => await handler(request, ${name}.patch, null, null${closestMiddleware ? `, ${closestMiddleware.name}` : ''}))`
+    if (exportedModules.includes('Patch')) {
+      methods += 'Patch, '
+      routesString += `.patch('${endpoint}', async (request: IttyRequest) => await handler(request, ${name}_Patch, null, null${closestMiddleware ? `, ${closestMiddleware.name}` : ''}))`
     }
   
-    if (typeof m.default.put === 'function') {
-      methods += 'PUT, '
-      routesString += `.put('${endpoint}', async (request: IttyRequest) => await handler(request, ${name}.put, null, null${closestMiddleware ? `, ${closestMiddleware.name}` : ''}))`
+    if (exportedModules.includes('Put')) {
+      methods += 'Put, '
+      routesString += `.put('${endpoint}', async (request: IttyRequest) => await handler(request, ${name}_Put, null, null${closestMiddleware ? `, ${closestMiddleware.name}` : ''}))`
     }
   
-    if (typeof m.default.delete === 'function') {
-      methods += 'DELETE, '
-      routesString += `.delete('${endpoint}', async (request: IttyRequest) => await handler(request, ${name}.delete, null, null${closestMiddleware ? `, ${closestMiddleware.name}` : ''}))`
+    if (exportedModules.includes('Delete')) {
+      methods += 'Delete, '
+      routesString += `.delete('${endpoint}', async (request: IttyRequest) => await handler(request, ${name}_Delete, null, null${closestMiddleware ? `, ${closestMiddleware.name}` : ''}))`
     }
   
-    if (typeof m.default.head === 'function') {
-      methods += 'HEAD, '
-      routesString += `.head('${endpoint}', async (request: IttyRequest) => await handler(request, ${name}.head, null, null${closestMiddleware ? `, ${closestMiddleware.name}` : ''}))`
+    if (exportedModules.includes('Head')) {
+      methods += 'Head, '
+      routesString += `.head('${endpoint}', async (request: IttyRequest) => await handler(request, ${name}_Head, null, null${closestMiddleware ? `, ${closestMiddleware.name}` : ''}))`
     }
   }
 
+  methods = methods
+    .substring(0, methods.length - 2)
+    .replace('Get', `Get as ${name}_Get`)
+    .replace('Post', `Post as ${name}_Post`)
+    .replace('Patch', `Patch as ${name}_Patch`)
+    .replace('Put', `Put as ${name}_Put`)
+    .replace('Delete', `Delete as ${name}_Delete`)
+    .replace('Head', `Head as ${name}_Head`)
+
+  // added route to imports string
+  importsString += `import { ${methods} } from "../src/${importPath.replace('/index', '')}"\n`
+
   // add endpoint for preflight request
-  if (config.handlePreflightRequests) routesString += `.options('${endpoint}', cors('${methods.substring(0, methods.length - 2)}'))`
+  if (config.handlePreflightRequests) routesString += `.options('${endpoint}', cors('${methods.toUpperCase()}'))`
 
   return {
     routes: routesString,
