75b9fb4c8e8ee396a0f558a8f707298b0fcba2c9
Azury <azury.wtf@gmail.com>
2022-05-18 23:33:47 +0200

    updated `Handler` type

--- packages/darkflare/types/modules/Handler.d.ts
+++ packages/darkflare/types/modules/Handler.d.ts
@@ -1,11 +1,4 @@
 import { DarkflareRequest } from './DarkflareRequest'
 import { DarkflareResponse } from './DarkflareResponse'
 
-export type Handler = {
-  get?: (req: DarkflareRequest, res: DarkflareResponse) => Promise<{ code?: number, [key: string]: any } | string | void>,
-  post?: (req: DarkflareRequest, res: DarkflareResponse) => Promise<{ code?: number, [key: string]: any } | string | void>,
-  patch?: (req: DarkflareRequest, res: DarkflareResponse) => Promise<{ code?: number, [key: string]: any } | string | void>,
-  put?: (req: DarkflareRequest, res: DarkflareResponse) => Promise<{ code?: number, [key: string]: any } | string | void>,
-  delete?: (req: DarkflareRequest, res: DarkflareResponse) => Promise<{ code?: number, [key: string]: any } | string | void>,
-  head?: (req: DarkflareRequest, res: DarkflareResponse) => Promise<{ code?: number, [key: string]: any } | string | void>
-}
+export type Handler = (req: DarkflareRequest, res: DarkflareResponse) => Promise<{ code?: number, [key: string]: any } | string | void>
