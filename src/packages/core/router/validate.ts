6a2568c6e4285f3ed5ad4ea21c7612c9ca4849ef
Azury <azury.wtf@gmail.com>
2022-07-03 17:37:46 +0200

    shortened variable names in `validate` module

--- src/packages/core/router/validate.ts
+++ src/packages/core/router/validate.ts
@@ -1,8 +1,8 @@
 import { TypeCompiler } from '@sinclair/typebox/compiler'
 import type { TSchema } from '@sinclair/typebox'
 
-export const validate = async (expected: TSchema, actual: unknown) => {
-  const compiledSchema = TypeCompiler.Compile(expected)
+export const validate = async (e: TSchema, a: unknown) => {
+  const compiledSchema = TypeCompiler.Compile(e)
 
-  return compiledSchema.Check(actual)
+  return compiledSchema.Check(a)
 }
