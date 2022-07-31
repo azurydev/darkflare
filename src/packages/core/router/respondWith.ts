2e94629e9119909a20c8c2c887ad128debdf81c6
Azury <azury.wtf@gmail.com>
2022-08-01 00:36:54 +0200

    chore: remove old new router

--- src/packages/core/router/respondWith.ts
+++
@@ -1,8 +0,0 @@
-export const respondWith = (message: string, code: number) => (
-  new Response(JSON.stringify({ code, message }), {
-    headers: {
-      'content-type': 'application/json; charset=utf-8'
-    },
-    status: code
-  })
-)
--- src/packages/core/router/validate.ts
+++
@@ -1,8 +0,0 @@
-import { TypeCompiler } from '@sinclair/typebox/compiler'
-import type { TSchema } from '@sinclair/typebox'
-
-export const validate = async (e: TSchema, a: unknown) => {
-  const compiledSchema = TypeCompiler.Compile(e)
-
-  return compiledSchema.Check(a)
-}
